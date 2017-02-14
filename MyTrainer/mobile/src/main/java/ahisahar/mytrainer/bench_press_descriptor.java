b5acb380ac88637d618f6dac26e27d6c04e48083
Ahisahar <approdriguez@correo.ugr.es>
2017-02-14 11:32:24 +0100

    bench finished ( padding left)

--- MyTrainer/mobile/src/main/java/ahisahar/mytrainer/bench_press_descriptor.java
+++ MyTrainer/mobile/src/main/java/ahisahar/mytrainer/bench_press_descriptor.java
@@ -2,12 +2,42 @@ package ahisahar.mytrainer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class bench_press_descriptor extends AppCompatActivity {
+
+    CarouselView carouselView;
+
+    int[] sampleImages = {R.drawable.bench0, R.drawable.bench1, R.drawable.bench2};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bench_press_descriptor);
+        setContentView(R.layout.activity_squat_descriptor);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        carouselView = (CarouselView) findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
+        //super.onBackPressed();
     }
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+    }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
+
+
 }
--- MyTrainer/mobile/src/main/java/ahisahar/mytrainer/squat_descriptor.java
+++ MyTrainer/mobile/src/main/java/ahisahar/mytrainer/squat_descriptor.java
@@ -2,12 +2,42 @@ package ahisahar.mytrainer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class squat_descriptor extends AppCompatActivity {
+
+    CarouselView carouselView;
+
+    int[] sampleImages = {R.drawable.squat0, R.drawable.squat1};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_squat_descriptor);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        carouselView = (CarouselView) findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
+        //super.onBackPressed();
     }
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+    }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
+
+
 }
