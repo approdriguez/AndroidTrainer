2c91bb842684f56eeb9be016da7f78d375c26796
Ahisahar <approdriguez@correo.ugr.es>
2017-02-21 13:30:42 +0100

    settings added

--- MyTrainer/mobile/src/main/java/ahisahar/mytrainer/MainActivity.java
+++ MyTrainer/mobile/src/main/java/ahisahar/mytrainer/MainActivity.java
@@ -2,6 +2,7 @@ package ahisahar.mytrainer;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -59,7 +60,30 @@ public class MainActivity extends AppCompatActivity {
         } else {
             mUserId = mFirebaseUser.getUid();
 
+            BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                    findViewById(R.id.bottom_navigation);
 
+            bottomNavigationView.setOnNavigationItemSelectedListener(
+                    new BottomNavigationView.OnNavigationItemSelectedListener() {
+                        @Override
+                        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                            switch (item.getItemId()) {
+                                case R.id.exercise:
+
+                                    break;
+                                case R.id.settings:
+
+                                    break;
+                                case R.id.main:
+
+                                    break;
+                            }
+                            return false;
+                        }
+                    });
+
+
+// Colors for selected (active) and non-selected items (in color reveal mode).
             ////////////////
             piechart = new PieChart(this);
             piechart = (PieChart) findViewById(R.id.chartactivity);
