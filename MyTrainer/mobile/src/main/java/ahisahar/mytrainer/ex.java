123e30ec666c3aba13f3c65e103aff5ffe201a7f
Ahisahar <approdriguez@correo.ugr.es>
2017-03-01 16:47:56 +0100

    exercises xml finished

---
+++ MyTrainer/mobile/src/main/java/ahisahar/mytrainer/ex.java
@@ -0,0 +1,12 @@
+package ahisahar.mytrainer;
+
+public class ex{
+    String name;
+    int photoID;
+
+    public ex(String name, int photoID){
+        this.name = name;
+        this.photoID = photoID;
+    }
+
+}
--- MyTrainer/mobile/src/main/java/ahisahar/mytrainer/exercises.java
+++ MyTrainer/mobile/src/main/java/ahisahar/mytrainer/exercises.java
@@ -1,13 +1,59 @@
 package ahisahar.mytrainer;
 
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class exercises extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class exercises extends Fragment {
+
+    public List<ex> exes = new ArrayList<>();
+
+
+    private View mContent;
+
+
+    public static Fragment newInstance() {
+        Fragment frag = new exercises();
+        Bundle args = new Bundle();
+        return frag;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_exercises);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_exercises, container, false);
+
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // initialize views
+        mContent = view.findViewById(R.id.fragment_content);
+
+
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+
+        super.onSaveInstanceState(outState);
+
+    }
+
+
+
 }
+
+
