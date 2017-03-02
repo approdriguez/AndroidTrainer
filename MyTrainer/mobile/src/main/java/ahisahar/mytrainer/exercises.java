2d5a90d253fead6352cb3ccefd629266479d302b
Ahisahar <approdriguez@correo.ugr.es>
2017-03-02 12:49:28 +0100

    finished exercises

--- MyTrainer/mobile/src/main/java/ahisahar/mytrainer/exercises.java
+++ MyTrainer/mobile/src/main/java/ahisahar/mytrainer/exercises.java
@@ -1,5 +1,6 @@
 package ahisahar.mytrainer;
 
+import android.content.Intent;
 import android.media.Image;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -45,16 +46,13 @@ public class exercises extends Fragment {
         ImageView image2 = (ImageView) view.findViewById(R.id.exercise_photo1);
         ImageView image3 = (ImageView) view.findViewById(R.id.exercise_photo2);
         ImageView image4 = (ImageView) view.findViewById(R.id.exercise_photo3);
-        ImageView image5 = (ImageView) view.findViewById(R.id.exercise_photo4);
+        //ImageView image5 = (ImageView) view.findViewById(R.id.exercise_photo4);
 
         TextView name1 = (TextView) view.findViewById(R.id.exercise_name);
         TextView name2 = (TextView) view.findViewById(R.id.exercise_name1);
         TextView name3 = (TextView) view.findViewById(R.id.exercise_name2);
         TextView name4 = (TextView) view.findViewById(R.id.exercise_name3);
         TextView name5 = (TextView) view.findViewById(R.id.exercise_name4);
-        // initialize views
-        mContent = view.findViewById(R.id.fragment_content);
-
         image1.setImageResource(R.drawable.bench0);
         name1.setText("Press de banca");
         image2.setImageResource(R.drawable.squat0);
@@ -63,10 +61,67 @@ public class exercises extends Fragment {
         name3.setText("Peso muerto");
         image4.setImageResource(R.drawable.squat0);
         name4.setText("Remo");
+        // initialize views
+
+        //click listener
+
+        image1.setOnClickListener(imgButtonHandler1);
+        image2.setOnClickListener(imgButtonHandler2);
+        image3.setOnClickListener(imgButtonHandler3);
+        image4.setOnClickListener(imgButtonHandler4);
+        mContent = view.findViewById(R.id.fragment_content);
+
+
 
 
 
     }
+
+    View.OnClickListener imgButtonHandler1 = new View.OnClickListener() {
+
+        public void onClick(View v) {
+
+            Intent intent = new Intent(getContext(),bench_press_descriptor.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+
+        }
+    };
+
+    View.OnClickListener imgButtonHandler2 = new View.OnClickListener() {
+
+        public void onClick(View v) {
+            Intent intent = new Intent(getContext(), squat_descriptor.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+
+
+        }
+    };
+
+    View.OnClickListener imgButtonHandler3 = new View.OnClickListener() {
+
+        public void onClick(View v) {
+            Intent intent = new Intent(getContext(), deadlift_descriptor.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+
+
+        }
+    };
+
+    View.OnClickListener imgButtonHandler4 = new View.OnClickListener() {
+
+        public void onClick(View v) {
+            Intent intent = new Intent(getContext(), row_descriptor.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+
+
+        }
+    };
+
+
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
