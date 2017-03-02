4051295cdfb75199d5e875fb11d81af5846a89c9
Ahisahar <approdriguez@correo.ugr.es>
2017-03-02 13:19:23 +0100

    history created

---
+++ MyTrainer/mobile/src/main/java/ahisahar/mytrainer/historico.java
@@ -0,0 +1,13 @@
+package ahisahar.mytrainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class historico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historico);
+    }
+}
