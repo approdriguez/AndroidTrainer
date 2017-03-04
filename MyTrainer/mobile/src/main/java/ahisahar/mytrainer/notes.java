93035ee31e916d77cae7d5757a6ab47d2c622569
Ahisahar <approdriguez@correo.ugr.es>
2017-03-04 19:35:06 +0100

    notes class extended from fragment

--- MyTrainer/mobile/src/main/java/ahisahar/mytrainer/notes.java
+++ MyTrainer/mobile/src/main/java/ahisahar/mytrainer/notes.java
@@ -1,13 +1,61 @@
 package ahisahar.mytrainer;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
 
-public class notes extends AppCompatActivity {
+import java.util.Calendar;
+
+public class notes extends Fragment {
+
+
+    private View mContent;
+
+
+    public static Fragment newInstance() {
+        Fragment frag = new historico();
+
+        Bundle args = new Bundle();
+        return frag;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notes);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_notes, container, false);
+
+
+
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mContent = view.findViewById(R.id.fragment_content);
+
+
+
+
+
+    }
+
+
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+
+        super.onSaveInstanceState(outState);
+
+    }
+
+
+
 }
